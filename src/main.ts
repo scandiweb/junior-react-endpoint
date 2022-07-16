a0525427d2145b78c798bc8687e26e9545c2b795
Filip Koza <38352025+Fil1989@users.noreply.github.com>
2022-07-16 21:03:05 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const server = new ApolloServer({
     resolvers: resolvers
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
